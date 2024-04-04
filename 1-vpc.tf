3335379467e2f684e318b0124cefe4629b3fa719
Nabeel Hamad <n.hamad@hotmail.co.uk>
2024-04-04 19:27:38 +0100

    add create manually deployed EKS

--- 1-vpc.tf
+++ 1-vpc.tf
@@ -28,7 +28,7 @@ resource "aws_subnet" "my_public_subnet_1" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "eu-west-2a"
+  availability_zone       = "us-east-1a"
 
   tags = {
     Name = "${var.name}_public_${var.region}"
@@ -46,7 +46,7 @@ resource "aws_subnet" "my_public_subnet_2" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "eu-west-2b"
+  availability_zone       = "us-east-1b"
 
   tags = {
     Name = "${var.name}_public_${var.region}"
@@ -66,7 +66,7 @@ resource "aws_subnet" "my_public_subnet_2" {
 resource "aws_subnet" "my_private_subnet_1" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "eu-west-2a"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "${var.name}_private_${var.region}"
@@ -83,7 +83,7 @@ resource "aws_subnet" "my_private_subnet_1" {
 resource "aws_subnet" "my_private_subnet_2" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.4.0/24"
-  availability_zone = "eu-west-2b"
+  availability_zone = "us-east-1b"
 
   tags = {
     Name = "${var.name}_private_${var.region}"
